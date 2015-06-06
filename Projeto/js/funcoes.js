0bfda3f5ab8668f84e25751d0fb6e37e7764badd
zanuzzorz <zanuzzorz@gmail.com>
2015-06-05 21:53:20 -0300

    Realizado ajuste na pagina inicial do Projeto e modificado pasta de documentação

--- Projeto/js/funcoes.js
+++ Projeto/js/funcoes.js
@@ -3,9 +3,12 @@ $(document).ready(function() {
   var owl = $("#owl_Produtos");
   owl.owlCarousel({
       //autoPlay: 5000, //Set AutoPlay to 3 seconds
-      items : 3,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3]
+
+      items : 1, //1 item above 1000px browser width
+      itemsDesktop : [5000,1], //5 items between 1000px and 901px
+      itemsDesktopSmall : [900,1], // betweem 900px and 601px
+      itemsTablet: [600,1], //2 items between 600 and 0
+      itemsMobile : [250,1] // itemsMobile disabled - inherit from itemsTablet option
  
   });
 
@@ -17,12 +20,24 @@ $(document).ready(function() {
     owl.trigger('owl.prev');
   })
  
-  $("#wl_Ingredientes").owlCarousel({
-      //autoPlay: 5000, //Set AutoPlay to 3 seconds
-      items : 1,
-      itemsDesktop : [1199,1],
-      itemsDesktopSmall : [979,1]
- 
+  $("#owl_Ingredientes").owlCarousel({
+    //autoPlay: 5000, //Set AutoPlay to 3 seconds
+    
+    items : 1, //1 item above 1000px browser width
+    itemsDesktop : [5000,1], //5 items between 1000px and 901px
+    itemsDesktopSmall : [900,1], // betweem 900px and 601px
+    itemsTablet: [600,1], //2 items between 600 and 0
+    itemsMobile : [250,1] // itemsMobile disabled - inherit from itemsTablet option
+  });
+
+  $("#owl_Ingredientes02").owlCarousel({
+    //autoPlay: 5000, //Set AutoPlay to 3 seconds
+
+    items : 1, //1 item above 1000px browser width
+    itemsDesktop : [5000,1], //5 items between 1000px and 901px
+    itemsDesktopSmall : [900,1], // betweem 900px and 601px
+    itemsTablet: [600,1], //2 items between 600 and 0
+    itemsMobile : [250,1] // itemsMobile disabled - inherit from itemsTablet option
   });
  
 });
