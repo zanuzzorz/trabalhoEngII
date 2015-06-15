4d9bf888de0acd7314752e334b8278c8fd1e149d
NatanJMai <natan.mai@hotmail.com>
2015-06-15 20:07:07 -0300

    Atualizacao cadastro de produtos [Natan, Mateus]

--- ConexaoBanco/ConexaoProduto.php
+++ ConexaoBanco/ConexaoProduto.php
@@ -1,44 +1,43 @@
 <?php
+   class ConexaoProduto extends ConexaoBase {
+	public function inserirProduto($produto){
+            $preco        = $produto->Preco;
+            $situacao     = $produto->Situacao;
+            $ingredientes = $produto->Ingredientes ;
+            $descricao    = $produto->Descricao;
+            $categoria    = $produto->Categoria;
 
-	class ConexaoProduto extends ConexaoBase {
-		public function inserirProduto($produto){
-                                    $preco           = $produto->Preco;
-                                    $situacao       = $produto->Situacao;
-                                    $ingredientes = $produto->Ingredientes ;
-                                    $descricao     = $produto->Descricao;
-                                    $categoria      = $produto->Categoria;
+            // Alterar o 1, é da categoria, FK.
+            // ID = NULL
+      	    $Sql          = "INSERT INTO `produto`  VALUES ('NULL', '$preco', '$situacao', '$ingredientes', '$descricao', '1')";
+	    return parent::insert($Sql);
+   	}
 
-                                    // Alterar o 1, é da categoria, FK.
-                                    // ID = NULL
-			$Sql                = "INSERT INTO `produto`  VALUES ('NULL', '$preco', '$situacao', '$ingredientes', '$descricao', '1')";
-			return parent::insert($Sql);
-		}
+  	//A função atualizar será nesse estilo
+  	/*public function atualizarProduto($obj){
+     	   $Sql="update produto set numero ='" . $obj->get_name() . "', complemento ='" . $obj->get_phone() . "', estado ='" . $obj->get_email() . "',";
+     	   $Sql.="bairro ='" . $obj->get_street() . "', rua =" . $obj->get_number();
+     	   return parent::update($Sql);
+  	}*/
 
-		//A função atualizar será nesse estilo
-		/*public function atualizarProduto($obj){
-			$Sql="update produto set numero ='" . $obj->get_name() . "', complemento ='" . $obj->get_phone() . "', estado ='" . $obj->get_email() . "',";
-			$Sql.="bairro ='" . $obj->get_street() . "', rua =" . $obj->get_number();
-			return parent::update($Sql);
-		}*/
+  	//A função buscarTodos será nesse estilo
+  	/*public function buscarTodos(){
+            $Sql="SELECT * FROM produto";
+     	    $Resultado = parent::select($Sql);
+     	    $Endereco = NULL;
+     	    $i = 0;
+     	    while ($linha = mysql_fetch_assoc($Resultado)) {
+ 		$Endereco[$i] = new \Entidades\Endereco();
+		$Endereco[$i]->set_isfree($linha["id"]);
+		$Endereco[$i]->set_code($linha["numero"]);
+		$Endereco[$i]->set_capacity($linha["complemento"]);
+		$Endereco[$i]->set_isfree($linha["estado"]);
+		$Endereco[$i]->set_isfree($linha["bairro"]);
+		$Endereco[$i]->set_isfree($linha["rua"]);
+		$i++;
+     	     }
+    	    return $Endereco;
+  	 }*/
 
-		//A função buscarTodos será nesse estilo
-		/*public function buscarTodos(){
-			$Sql="SELECT * FROM produto";
-			$Resultado = parent::select($Sql);
-			$Endereco = NULL;
-			$i = 0;
-			while ($linha = mysql_fetch_assoc($Resultado)) {
-				$Endereco[$i] = new \Entidades\Endereco();
-				$Endereco[$i]->set_isfree($linha["id"]);
-				$Endereco[$i]->set_code($linha["numero"]);
-				$Endereco[$i]->set_capacity($linha["complemento"]);
-				$Endereco[$i]->set_isfree($linha["estado"]);
-				$Endereco[$i]->set_isfree($linha["bairro"]);
-				$Endereco[$i]->set_isfree($linha["rua"]);
-				$i++;
-			}
-			return $Endereco;
-		}*/
-
-	}
+   }
 ?>
