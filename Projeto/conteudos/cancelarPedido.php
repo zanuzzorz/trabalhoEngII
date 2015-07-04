79754570fbffdb17b54c3d7b346bf1556d5b80fd
zanuzzorz <zanuzzorz@gmail.com>
2015-07-04 08:26:44 -0300

    Modificado cancelar Pedido

--- Projeto/conteudos/cancelarPedido.php
+++ Projeto/conteudos/cancelarPedido.php
@@ -42,7 +42,8 @@
 
   if($_SESSION['tipo'] == 1){ //Mesa
     echo "<meta http-equiv='refresh' content='0; url=acompanharPedido.php?cancelado=1'>";
+  }else{
+    echo "<meta http-equiv='refresh' content='0; url=cliente.php?cancelado=1'>";
   }
              
-  echo "<meta http-equiv='refresh' content='0; url=cliente.php?cancelado=1'>";
 ?>
